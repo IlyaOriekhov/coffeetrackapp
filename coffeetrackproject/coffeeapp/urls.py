03426a8666f5a73319eb3203332148e423a52d56
Illia <ilya.orekhov@ukr.net>
2025-11-23 18:41:10 +0200

    make homepage

--- coffeetrackproject/coffeeapp/urls.py
+++ coffeetrackproject/coffeeapp/urls.py
@@ -2,15 +2,16 @@ from django.urls import path
 from .views import (
     CoffeeListView, CoffeeCreateView, CoffeeDetailView,
     CoffeeUpdateView, CoffeeDeleteView, RecipeListView, RecipeCreateView, RecipeDetailView,
-    RecipeUpdateView, RecipeDeleteView, login_view, register_view, logout_view
+    RecipeUpdateView, RecipeDeleteView, login_view, register_view, logout_view, home_view
 )
 
 urlpatterns = [
-    path('', CoffeeListView.as_view(), name='coffee_list'),
-    path('create/', CoffeeCreateView.as_view(), name='coffee_create'),
-    path('<int:pk>/', CoffeeDetailView.as_view(), name='coffee_detail'),
-    path('<int:pk>/edit/', CoffeeUpdateView.as_view(), name='coffee_edit'),
-    path('<int:pk>/delete/', CoffeeDeleteView.as_view(), name='coffee_delete'),
+    path('', home_view, name='home'),
+    path('coffees/', CoffeeListView.as_view(), name='coffee_list'),
+    path('coffees/create/', CoffeeCreateView.as_view(), name='coffee_create'),
+    path('coffees/<int:pk>/', CoffeeDetailView.as_view(), name='coffee_detail'),
+    path('coffees/<int:pk>/edit/', CoffeeUpdateView.as_view(), name='coffee_edit'),
+    path('coffees/<int:pk>/delete/', CoffeeDeleteView.as_view(), name='coffee_delete'),
 
     path('recipes/', RecipeListView.as_view(), name='recipe_list'),
     path('recipes/add/', RecipeCreateView.as_view(), name='recipe_add'),
--- coffeetrackproject/coffeeapp/views.py
+++ coffeetrackproject/coffeeapp/views.py
@@ -52,7 +52,7 @@ class CoffeeDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['recipes'] = Recipe.objects.filter(coffee=self.object)
+        context["recipes"] = Recipe.objects.filter(coffee=self.object)
         return context
 
 
@@ -177,3 +177,6 @@ def logout_view(request):
     logout(request)
     messages.success(request, "Logout successful!")
     return redirect("coffee_list")
+
+def home_view(request):
+    return render(request, "home.html")
